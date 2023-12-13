39e4d8b5821f9d7f9800233f65977bbaa2fabc60
nikkamay <91564366+nikkamay@users.noreply.github.com>
2023-12-13 16:12:29 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -39,7 +39,7 @@ def addWorker(token, num):
       tdata=json.load(p)
     tdata['name']='slave'+str(num)
     data=json.dumps(tdata)
-    url='https://www.googleapis.com/compute/v1/projects/automatic-time-40031/zones/europe-west1-b/instances'
+    url='https://www.googleapis.com/compute/v1/projects/automatic-time-400314/zones/europe-west1-b/instances'
     headers={"Authorization": "Bearer "+token}
     resp=requests.post(url,headers=headers, data=data)
     if resp.status_code==200:     
